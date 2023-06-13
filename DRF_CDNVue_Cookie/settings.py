72265cc5aba72c44e7a0119e5ef5b313c99a65e7
RyoHatano <ryohatano0809@gmail.com>
2023-06-13 22:49:04 +0900

    additional settings

--- DRF_CDNVue_Cookie/settings.py
+++ DRF_CDNVue_Cookie/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # 3rd party apps
+    'rest_framework',
+    
+    # My applications
+    'shop.apps.ShopConfig',
+    'apiv1.apps.Apiv1Config',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = "DRF_CDNVue_Cookie.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],   # テンプレトートファイルの検索ディレクトリに「<ベースディレクトリ>/templates」を指定
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -97,11 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -109,9 +118,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIR = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Authentication ログイン・ログアウト成功時のリダイレクト先を変更
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "rest_framework:login"
+
+
+# DjangoRESTFramework DRFのCookie認証の設定
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
